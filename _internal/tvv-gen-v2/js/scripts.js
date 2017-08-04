0b3bead886de450ed13b855f969e3f480882b06c
m@rtlin2cz <martlin2cz@gmail.com>
2017-08-04 11:46:04 +0200

    Installed newest versions of oic and streeting (into tvv-gen-v2)
    	- updated oic to currently latest version
    	- updated streeting in tvv-gen-v2 to currently lastest version
    	- correspodingly modified tvv-gen-v2 to be able to work with new streeting
    	- image url inputs in streeting replaced by buttons and prompts

--- _internal/tvv-gen-v2/js/scripts.js
+++ _internal/tvv-gen-v2/js/scripts.js
@@ -17,29 +17,46 @@ function initialize() {
  var form = document.getElementById(FORM_ID);                                                                                         
    
   form.onsubmit = function(e) {   
-    update();
+    updateComplete();
     return false;
   }
       
   streeting.initialize('output-svg', template);
-  streeting.makeSourceInteractive(FORM_ID, update);
+  streeting.makeSourceInteractive(FORM_ID, updateOne);
 
 
 }
 
-
-function update() {
+function updateComplete() {
   var links = streeting.process('output-svg', FORM_ID);
 
 	var svgLink = links.svg;
 	
 	var link = document.getElementById('output-link');
 	link.href = svgLink;
-
-//XXX  generateOutputs();
 }
 
+function updateOne(event) {
+	var sender = event.target;
+  
+	streeting.processUpdate('output-svg', sender, function(link) {
+		var outlink = document.getElementById('output-link');
+		outlink.href = link;
+	});
+}
 
+function promptImage(event) {
+	var source = event.target;
+	var input = source.previousElementSibling;
+
+	var url = prompt('Zadej adresu obrázku');
+	if (url) {
+		input.value = url;
+
+		var evtHack = { 'target': input };
+		updateOne(evtHack);
+	}
+}
 /////////////////////////////////////////////////////////////////////
 function cloneAsSibling(className, count) {
 	var items = document.getElementsByClassName(className);
@@ -79,7 +96,7 @@ function setupCropperLinks() {
 		
 		input.addEventListener('input', function(e) {
 			var input = e.target;
-			var link = input.nextSibling.nextSibling;
+			var link = input.nextElementSibling.nextElementSibling;
 
 			var url = input.value;	
 			link.href = "../online-image-cropper/?img=" + url;
@@ -100,140 +117,3 @@ function putDate(inputIdSpec, daysAfter) {
 /////////////////////////////////////////////////////////////////////
 
 
-/*
-function formToJSON() {
-	return {
-		'title':		inputElem('title').value,
-		'subject':	inputElem('subject').value,
-		'target':		inputElem('target').value,
-		'duration':	inputElem('duration').value,
-		'annotation':		inputElem('annotation').value,
-		'authorName':	inputElem('author-name').value,
-		'authorDescription':	inputElem('author-description').value,
-		'background': 	inputElem('background').value,
-		'authorThumbnail':		inputElem('author-thumbnail').value,
-		'timestamp': 		new Date().toString()	
-	};
-}
-
-
-function jsonToForm(json) {
-	inputElem('title').value 		= json.title;
-	inputElem('subject').value	= json.subject;
-	inputElem('target').value		= json.target;
-	inputElem('duration').value	= json.duration;
-	inputElem('annotation').value		= json.annotation;
-	inputElem('author-name').value	= json.authorName;
-	inputElem('author-description').value = json.authorDescription;
-	inputElem('background').value		= json.background;
-	inputElem('author-thumbnail').value = json.authorThumbnail;
-}
-*/
-
-/////////////////////////////////////////////////////////////////////
-/*
-
-
-function transferDate(idSpec, withYear) {
-	var input = inputElem(idSpec);
-	var output = outputElem(idSpec);
-
-	var value = input.valueAsDate;
-	var text = "";
-	if (value) {
-		text = 
-			(value.getDate()) + "." +
-			(value.getMonth() + 1) + "." + 
-			(withYear ? value.getFullYear() : '');
-	}
-
-	output.innerHTML = text;
-}
-
-function transferPost(index) {
-	var inputImg = inputElemClassed("post-image", index);
-	var inputTitle = inputElemClassed("post-title", index);
-	var inputCrop = inputElemClassed("post-crop", index);
-
-	var outputImg = outputElemClassed("post-image", index);
-	var outputTitle = outputElemClassed("post-title", index);
-
-	var img = inputImg.value;
-	if (img == "") {
-		img = "img/no-image.svg";
-	}
-	outputImg.src = img;
-
-	var title = inputTitle.value;
-	outputTitle.innerHTML = title;
-
-	if (inputCrop.checked) {
-		outputImg.classList.add("rounded");
-	} else {
-		outputImg.classList.remove("rounded");
-	}
-	
-}
-
-/////////////////////////////////////////////////////////////////////
-
-function exportJSON() {
-	var exportId = "export-json";
-	var exportElem = document.getElementById(exportId);
-
-	var json = formToJSON();
-	var str = JSON.stringify(json);
-
-	exportElem.value = str;
-}
-
-function importJSON() {
-	var importId = "import-json";
-	var importElem = document.getElementById(importId);
-
-	var str = importElem.value;
-	
-	var json;
-	try {
-		json = JSON.parse(str);
-	} catch(e) {
-		alert("Chybný formát dat: " + e);
-		return;
-	}
-
-	jsonToForm(json);
-	update();
-}
-
-/////////////////////////////////////////////////////////////////////
-
-
-function inputElem(idSpec) {
-	var inputId = "input-" + idSpec;
-	var input = document.getElementById(inputId);
-	return input;
-}
-
-function outputElem(idSpec) {
-	var outputId = "output-" + idSpec;
-	var output = document.getElementById(outputId);
-	return output;
-}
-
-function inputElemClassed(classSpec, index) {
-	var inputClass = "input-" + classSpec;
-	var inputs = document.getElementsByClassName(inputClass);
-	var input = inputs[index - 1];
-	return input;
-}
-
-function outputElemClassed(classSpec, index) {
-	var outputClass = "output-" + classSpec;
-	var outputs = document.getElementsByClassName(outputClass);
-	var output = outputs[index - 1];
-	return output;
-}
-
-*/
-
-	
