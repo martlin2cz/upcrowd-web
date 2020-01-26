12da4b22818df8a85e0a81130698e63a07abc31e
martlin2cz2 <martlin@seznam.cz>
2020-01-26 22:35:10 +0100

    Added katalog facebook metadata
     - katalog placeholder page made html redirecting instead of server one

--- files/aktualni-katalog-prednasek.php
+++ files/aktualni-katalog-prednasek.php
@@ -1,3 +1,18 @@
 <?php
-	header("Location: " . "UPC-Katalog-07.10.2019-V6.pdf");
+	$file="UPC-Katalog-07.10.2019-V6.pdf";
+?>
+<html>
+	<head>
+		<meta property="og:url" content="http://upcrowd.upol.cz/files/<?= $file ?>" />
+		<meta property="og:type" content="article" />
+		<meta property="og:title" content="Katalog přednášek" />
+		<meta property="og:description" content="Katalog popularizačních přednášek pro střední školy - 2020" />
+		<meta property="og:image" content="http://upcrowd.upol.cz/files/katalog-nahled.png" />
+
+		<meta HTTP-EQUIV="REFRESH" content="0; url=http://upcrowd.upol.cz/files/<?= $file ?>" />
+	</head>
+	<body>
+		<a href="<?= $file ?>">Katalog přednášek</a>
+	</body>
+</html>
 
